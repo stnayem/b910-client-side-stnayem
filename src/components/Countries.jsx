c6a5be7b9b35c3e3f54adfab52767c9a55d0e193
stnayem <stnayem@gmail.com>
2024-07-23 15:02:19 +0800

    country section updated

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -5,18 +5,19 @@ import spain from '../assets/europe-imgs/07_Sagrada_Família_Barcelona.png';
 import england from '../assets/europe-imgs/12_Stonehenge_Wiltshire.jpg';
 import netherlands from '../assets/europe-imgs/13_rijksmuseum-i-amsterdam-hd.jpg';
 import switzerland from '../assets/europe-imgs/18_Lake_Geneva_Geneva.png';
+import { Link } from "react-router-dom";
 
 const Countries = () => {
     return (
         <div>
             <h2 className="h3-home">Trending Destination</h2>
             <div className="grid sm:gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-                <CountryCard name="France" img={france} />
-                <CountryCard name="Italy" img={italy} />
-                <CountryCard name="Spain" img={spain} />
-                <CountryCard name="England" img={england} />
-                <CountryCard name="Netherlands" img={netherlands} />
-                <CountryCard name="Switzerland" img={switzerland} />
+                <Link to={`/country/france`} className="bg-red-300"><CountryCard name="France" img={france} /></Link>
+                <Link to={`/country/italy`} className="bg-red-300"><CountryCard name="Italy" img={italy} /></Link>
+                <Link to={`/country/spain`} className="bg-red-300"><CountryCard name="Spain" img={spain} /></Link>
+                <Link to={`/country/england`} className="bg-red-300"><CountryCard name="England" img={england} /></Link>
+                <Link to={`/country/netherlands`} className="bg-red-300"><CountryCard name="Netherlands" img={netherlands} /></Link>
+                <Link to={`/country/switzerland`} className="bg-red-300"><CountryCard name="Switzerland" img={switzerland} /></Link>
             </div>
         </div>
     );
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -2,12 +2,14 @@
 
 const CountryCard = ({ name, img }) => {
     return (
-        <div>
-            <p>{name}</p>
-            <div className="">
-                <img src={img} alt="" />
+        <div className="relative h-full">
+            <div className="relative z-[1] w-full h-full bg-fuchsia-600">
+                <img src={img} alt="" className="h-full" />
 
             </div>
+            <p className="h3-home absolute bottom-0 left-0 z-[2] p-5 w-full text-center text-white bg-black bg-opacity-50">
+                {name}
+            </p>
 
         </div>
     );
